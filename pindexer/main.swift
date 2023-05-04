3ebe9b095a4557c5afffbb031092ac3283dbd98c
Konstantin Gonikman <gonikman@me.com>
2023-05-05 01:00:55 +0200

    get embeddings

--- pindexer/main.swift
+++ pindexer/main.swift
@@ -49,6 +49,79 @@ func numOfTokens(fileContent: String) -> Int {
     return num
 }
 
+func callAPI(text: String) {
+    // Define the URL and create the URL object
+    guard let url = URL(string: "https://api.openai.com/v1/embeddings") else {
+        print("Invalid URL")
+        exit(1) // Exit with an error code
+    }
+
+    // Create the JSON data.
+    let jsonData: [String: Any] = [
+        "input": text,
+        "model": "text-embedding-ada-002"
+    ]
+
+    do {
+        // Convert the JSON data to Data object
+        let postData = try JSONSerialization.data(withJSONObject: jsonData, options: [])
+
+        // Create a URLRequest object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("Bearer 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", forHTTPHeaderField: "Authorization")
+        request.httpBody = postData
+
+        // Create a DispatchSemaphore
+        let semaphore = DispatchSemaphore(value: 0)
+
+        // Create a URLSessionDataTask to send the request
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            // Handle response and errors
+            if let error = error {
+                print("Error: \(error.localizedDescription)")
+                semaphore.signal() // Signal that the request is complete
+                return
+            }
+
+            if let data = data {
+                do {
+                    // Parse the JSON data and print it
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+
+                    if let jsonObject = json as? [String: Any] {
+                        let data = jsonObject["data"] as! [Any]
+
+                        if let dataObj = data[0] as? [String: Any],
+                        let embeddings = dataObj["embedding"] as? [Double] {
+//                            print("embeddings: \(embeddings)")
+                            processEmbeddings(embeddings)
+                        }
+                    }
+
+//                    print("JSON: \(json)")
+                } catch {
+                    print("Error parsing JSON: \(error.localizedDescription)")
+                }
+            }
+            semaphore.signal() // Signal that the request is complete
+        }
+
+        task.resume() // Start the task
+
+        // Wait for the semaphore to be signaled
+        semaphore.wait()
+
+    } catch {
+        print("Error creating JSON data: \(error.localizedDescription)")
+    }
+}
+
+func processEmbeddings(_ embeddings: [Double]) {
+
+}
+
 if let files = filesInDirectory(atPath: "/Users/kostik/Library/Mobile Documents/iCloud~md~obsidian/Documents/tmp", withExtension: "md") {
     print("Found \(files.count) md files:")
     for file in files {
@@ -56,6 +129,7 @@ if let files = filesInDirectory(atPath: "/Users/kostik/Library/Mobile Documents/
         let content = loadContent(atPath: file.path(percentEncoded: false))
         let tokens = numOfTokens(fileContent: content)
         print(tokens)
+        callAPI(text: content)
     }
 } else {
     print("Failed to find files")
