945a00cac58bdac6602e85b06daccc37442b295e
Konstantin Gonikman <gonikman@me.com>
2023-06-22 20:15:31 +0200

    now it's a menu app, no more dock

--- AssistAI/AppDelegate.swift
+++ AssistAI/AppDelegate.swift
@@ -7,10 +7,82 @@
 
 import Cocoa
 
-@main
-class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate {
+
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.variableLength)
+    var window: NSWindow!
+
+    private func constructMenu() {
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "mountain.2.fill", accessibilityDescription: nil)
+        }
+
+        let menu = NSMenu()
+
+        let mStatus = NSMenuItem(title: "Lumistic is running", action: nil, keyEquivalent: "")
+        mStatus.isEnabled = false
+
+        var statusConfig = NSImage.SymbolConfiguration(textStyle: .body,
+                                                       scale: .large)
+        statusConfig = statusConfig.applying(.init(hierarchicalColor: .systemGreen))
+        mStatus.image = NSImage(systemSymbolName: "circlebadge.fill", accessibilityDescription: nil)?
+            .withSymbolConfiguration(statusConfig)
+        menu.addItem(mStatus)
+        menu.addItem(NSMenuItem.separator())
+
+        menu.addItem(NSMenuItem(title: "Open Lumistic", action: #selector(AppDelegate.didTapOpenMainWindow(_:)), keyEquivalent: ""))
+        menu.addItem(NSMenuItem(title: "View Session Logs...", action: #selector(AppDelegate.didTapViewSessionLogs(_:)), keyEquivalent: ""))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Indexer", action: #selector(AppDelegate.didTapQuit(_:)), keyEquivalent: ""))
+
+        statusItem.menu = menu
+    }
+
+    @objc func didTapOpenMainWindow(_ sender: Any?) {
+        if let window = window, window.isVisible {
+            return
+        }
+
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let windowController = storyboard.instantiateInitialController() as?
+                NSWindowController else { return }
+
+        guard let window = windowController.window else {
+            return
+        }
+
+        self.window = window
+
+        NSApp.activate(ignoringOtherApps: true)
+        window.makeKeyAndOrderFront(self)
+    }
+
+    @objc func didTapViewSessionLogs(_ sender: Any?) {
+        print("View logs...")
+    }
+
+    @objc func didTapQuit(_ sender: Any?) {
+        let alert = NSAlert()
+        alert.messageText = "Stop indexing?"
+        alert.informativeText = "Lumistic is the background process that indexes your files. Quitting it means all indexing activity will stop."
+        alert.addButton(withTitle: "Don't Quit")
+        alert.addButton(withTitle: "Quit")
+
+        let modalResult = alert.runModal()
+
+        switch modalResult {
+        case .alertFirstButtonReturn:
+            print("Cancel button clicked")
+        case .alertSecondButtonReturn:
+            NSApplication.shared.terminate(self)
+        default:
+            break
+        }
+    }
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        constructMenu()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ AssistAI/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  tmpd
+//
+//  Created by Konstantin Gonikman on 22.06.23.
+//
+
+import Cocoa
+
+let app = NSApplication.shared
+let delegate = AppDelegate()
+app.delegate = delegate
+_ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
