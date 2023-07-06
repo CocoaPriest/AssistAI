83035c5c9b25c9433cd05b8449431704e152b319
Konstantin Gonikman <gonikman@me.com>
2023-07-06 15:59:35 +0200

    folder selection

--- AssistAI/SettingsViewController.swift
+++ AssistAI/SettingsViewController.swift
@@ -9,15 +9,26 @@ import Cocoa
 import os.log
 import ServiceManagement
 
+struct PathInfo {
+    let url: URL
+    let numberOfFiles: Int
+}
+
 final class SettingsViewController: NSViewController {
 
+    @IBOutlet weak var btnRemoveFolder: NSButton!
     @IBOutlet weak var tableView: NSTableView!
     @IBOutlet weak var btnAutologin: NSButton!
     private let loginItem = SMAppService.mainApp
+    private var urls: [PathInfo] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        btnRemoveFolder.isEnabled = false
+
+        tableView.dataSource = self
+        tableView.delegate = self
         btnAutologin.state = (loginItem.status == .enabled) ? .on : .off
     }
     
@@ -28,14 +39,40 @@ final class SettingsViewController: NSViewController {
         folderSelectionDialog.allowsMultipleSelection = true
         folderSelectionDialog.prompt = "Select" // button
         folderSelectionDialog.message = "Please select one or more folders"
-        folderSelectionDialog.beginSheetModal(for: self.view.window!) { response in
+        folderSelectionDialog.beginSheetModal(for: self.view.window!) {  [unowned self] response in
             if response == NSApplication.ModalResponse.OK {
                 print("User selected folder: \(folderSelectionDialog.urls)")
+                let pathInfos = folderSelectionDialog.urls.compactMap { url in
+                    PathInfo(url: url, numberOfFiles: 0)
+                }
+
+                urls.append(contentsOf: pathInfos)
+
+                // TODO: don't let the user make overlapping selections:
+                // - If a subfolder already selected -> take root folder, remove this subfolder
+                // - If a root folder already selected -> ignore any subfolder selection
+                tableView.reloadData()
             }
         }
     }
 
     @IBAction func didTapRemoveFolder(_ sender: Any) {
+        guard tableView.selectedRow >= 0 else {
+            return
+        }
+
+        let alert = NSAlert()
+        alert.messageText = "Remove folder?"
+        alert.informativeText = "This removes this folder from being indexed."
+        alert.addButton(withTitle: "Cancel")
+        alert.addButton(withTitle: "Remove")
+
+        let modalResult = alert.runModal()
+        if modalResult == .alertSecondButtonReturn {
+            urls.remove(at: tableView.selectedRow)
+            btnRemoveFolder.isEnabled = false
+            tableView.reloadData()
+        }
     }
 
     @IBAction func didTapExclude(_ sender: Any) {
@@ -70,5 +107,29 @@ final class SettingsViewController: NSViewController {
         }
 
         btnAutologin.state = (loginItem.status == .enabled) ? .on : .off
+    }
+}
+
+extension SettingsViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return urls.count
+    }
+
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        guard let ident = tableColumn?.identifier else {
+            return nil
+        }
+
+        if ident.rawValue == "cPath" {
+            return urls[row].url.path(percentEncoded: false)
+        } else {
+            return urls[row].numberOfFiles
+        }
+    }
+}
+
+extension SettingsViewController: NSTableViewDelegate {
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        btnRemoveFolder.isEnabled = self.tableView.selectedRow >= 0
     }
 }
